fc1165c93c51c108307ab22215f737f8bf4ae683
RomanecTatyana <romanectatyana@gmail.com>
2026-07-14 21:25:30 +0300

    Introduce ItemsViewModel for items window

---
+++ Accounting.Wpf/Models/ItemRow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accounting.Wpf.Models
+{
+    public class ItemRow
+    {
+        public string Code { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Unit { get; set; } = "";
+        public string Group { get; set; } = "";
+    }
+}
---
+++ Accounting.Wpf/ViewModel/ItemsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+using Accounting.Wpf.Models;
+
+namespace Accounting.Wpf.ViewModels
+{
+    public class ItemsViewModel
+    {
+        public ObservableCollection<ItemRow> Items { get; set; }
+
+        public ItemsViewModel()
+        {
+            Items = new ObservableCollection<ItemRow>
+            {
+                new ItemRow
+                {
+                    Code = "001",
+                    Name = "Свинина",
+                    Unit = "кг",
+                    Group = "Сировина"
+                },
+                new ItemRow
+                {
+                    Code = "002",
+                    Name = "Яловичина",
+                    Unit = "кг",
+                    Group = "Сировина"
+                },
+                new ItemRow
+                {
+                    Code = "003",
+                    Name = "Сіль",
+                    Unit = "кг",
+                    Group = "Матеріали"
+                }
+            };
+        }
+    }
+}
