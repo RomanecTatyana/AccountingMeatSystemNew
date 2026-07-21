6b35623f6dcc814e8cbfb562f73e527f96f0919b
RomanecTatyana <romanectatyana@gmail.com>
2026-07-21 12:24:40 +0300

    Add MainViewModel and keep items screen on ViewModel

--- Accounting.Wpf/MainWindow.xaml.cs
+++ Accounting.Wpf/MainWindow.xaml.cs
@@ -1,25 +1,17 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Accounting.Wpf.ViewModel;
 
 namespace Accounting.Wpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainViewModel();
         }
+
         private void OpenReceiptWindow_Click(object sender, RoutedEventArgs e)
         {
             ReceiptWindow receiptWindow = new ReceiptWindow();
@@ -27,11 +19,9 @@ namespace Accounting.Wpf
         }
 
         private void OpenItemsWindow_Click(object sender, RoutedEventArgs e)
-        { 
+        {
             ItemsWindow itemsWindow = new ItemsWindow();
             itemsWindow.Show();
         }
     }
-
-
 }
---
+++ Accounting.Wpf/ViewModel/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accounting.Wpf.ViewModel
+{
+    public class MainViewModel
+    {
+        public string Title { get; set; } = "Accounting Application";
+    }
+}
