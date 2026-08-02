19e1e565dc08d74f28cd4a785a77e7bef14484ff
RomanecTatyana <romanectatyana@gmail.com>
2026-08-02 20:50:09 +0300

    Organize WPF project structure

--- Accounting.Wpf/Views/ReceiptWindow.xaml.cs
+++ Accounting.Wpf/Views/ReceiptWindow.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace Accounting.Wpf
+namespace Accounting.Wpf.Views
 {
     /// <summary>
     /// Interaction logic for ReceiptWindow.xaml
