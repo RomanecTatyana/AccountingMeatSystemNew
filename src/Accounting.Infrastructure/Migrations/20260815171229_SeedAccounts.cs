9340a2a54b6e36d5e666aaea77531ef9284a733d
RomanecTatyana <romanectatyana@gmail.com>
2026-08-15 20:32:48 +0300

    Seed initial accounting accounts

---
+++ src/Accounting.Infrastructure/Migrations/20260815171229_SeedAccounts.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Accounting.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAccounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "Id", "Code", "Name" },
+                values: new object[,]
+                {
+                    { 1, "201", "Сировина і матеріали" },
+                    { 2, "23", "Виробництво" },
+                    { 3, "26", "Готова продукція" },
+                    { 4, "361", "Розрахунки з покупцями" },
+                    { 5, "631", "Розрахунки з постачальниками" },
+                    { 6, "641", "Розрахунки за податками" },
+                    { 7, "701", "Дохід від реалізації" },
+                    { 8, "901", "Собівартість реалізації" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 8);
+        }
+    }
+}
