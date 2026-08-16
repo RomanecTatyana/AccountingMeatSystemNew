ca98c69bdd7c9c85f10ca9d57616f463bbfa308c
RomanecTatyana <romanectatyana@gmail.com>
2026-08-16 18:18:53 +0300

    Add warehouse API endpoints

---
+++ src/Accounting.Infrastructure/Repositories/WarehouseRepository.cs
@@ -0,0 +1,43 @@
+﻿using Accounting.Domain.Entities;
+using Accounting.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accounting.Infrastructure.Repositories
+{
+    public class WarehouseRepository
+    {
+        private readonly AppDbContext db;
+        public WarehouseRepository(AppDbContext db)
+        {
+            this.db = db;
+        }
+
+        public List<Warehouse> GetAll()
+        {
+            return db.Warehouses
+                .OrderBy(warehouse => warehouse.Code)
+                .ToList();
+        }
+
+        public Warehouse? GetByCode(string code)
+        {
+            return db.Warehouses
+                .FirstOrDefault(warehouse => warehouse.Code == code);
+        }
+
+        public bool ExistsByCode(string code)
+        {
+            return db.Warehouses
+                .Any(warehouse => warehouse.Code == code);
+        }
+
+        public void Add(Warehouse warehouse)
+        {
+            db.Warehouses.Add(warehouse);
+            db.SaveChanges();
+        }
+
+    }
+}
